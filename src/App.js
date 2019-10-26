730ce98d82c86f257de393a4e737fe019f40718d
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-26 22:39:25 +0900

    feat(coding): とりあえず写経

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todos: [
+        {id: "1", text: "todo1"},
+        {id: "2", text: "todo2"},
+        {id: "3", text: "todo3"}
+      ]
+    };
+  }
+
+    deleteTodo(id){
+      let todos = this.state.todos;
+      todos = todos.filter((todo) => todo.id !== id);
+
+      this.setState({todos: todos});
+    };
+
+    render(){
+      return(
+        <ul>
+          {
+            this.state.todos.map(
+              (todo) => {
+                return (
+                  <li key={todo.id}>{todo.text}
+                    <button onClick={this.state.deleteTodo.bind(this, todo.id)}>×</button>
+                  </li>
+                );
+              }
+            )
+          }
+        </ul>
+      );
+    }
+  }
 
 export default App;
